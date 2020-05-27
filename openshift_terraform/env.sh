9fc8d97bf04db1c627bf7c2aefb24ecb77a7d35d
Matthew Wilkerson <mwilkers@redhat.com>
2020-05-27 07:34:29 -0600

    Helper script to load default ENV variables from ~/.aws/credentials

---
+++ openshift_terraform/env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# USAGE: source env.sh
+
+# If AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are NOT set, use the default creds from ~/.aws/credentials
+if [ -z "${AWS_ACCESS_KEY_ID}" ] && [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+  export AWS_ACCESS_KEY_ID="$(aws configure get default.aws_access_key_id)"
+  export AWS_SECRET_ACCESS_KEY="$(aws configure get default.aws_secret_access_key)"
+fi
+
+if [ -z "${AWS_ACCESS_KEY_ID}" ] && [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+  echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not set"
+  echo "Tried to obtain them from ~/.aws/credentials but failed"
+  echo "Ensure that you have the above ENV variables set or a ~/.aws/credentials file"
+
+else
+  echo "AWS Keys exported successfully"
+  env | grep AWS
+fi
