459b1c3d7ceac565ebe4e8405fea1c494f25493b
Matthew Wilkerson <mwilkers@redhat.com>
2021-05-17 06:59:24 -0700

    older version of sops.py

--- openshift4_aws/vars_plugins/sops.py
+++ openshift4_aws/vars_plugins/sops.py
@@ -1,105 +1,78 @@
-# -*- coding: utf-8 -*-
-#
-#  Copyright 2018 Edoardo Tenani <e.tenani@arduino.cc> [@endorama]
-#
-# This file is part of Ansible.
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+
 DOCUMENTATION = '''
-    name: sops
-    author: Edoardo Tenani (@endorama) <e.tenani@arduino.cc>
-    short_description: Loading sops-encrypted vars files
-    version_added: '0.1.0'
+    vars: sops
+    version_added: "N/A"
+    short_description: In charge of loading SOPS-encrypted vars
     description:
-        - Load encrypted YAML files into corresponding groups/hosts in group_vars/ and host_vars/ directories.
-        - Files are encrypted prior to reading, making this plugin an effective companion to host_group_vars plugin.
-        - Files are restricted to .sops.yaml, .sops.yml, .sops.json extensions.
-        - Hidden files are ignored.
-    options:
-      _valid_extensions:
-        default: [".sops.yml", ".sops.yaml", ".sops.json"]
-        description:
-          - "Check all of these extensions when looking for 'variable' files which should be YAML or JSON or vaulted versions of these."
-          - 'This affects vars_files, include_vars, inventory and vars plugins among others.'
-        type: list
-      stage:
-        version_added: 0.2.0
-        ini:
-          - key: vars_stage
-            section: community.sops
-        env:
-          - name: ANSIBLE_VARS_SOPS_PLUGIN_STAGE
-      cache:
-        description:
-          - Whether to cache decrypted files or not.
-          - If the cache is disabled, the files will be decrypted for almost every task. This is very slow!
-          - Only disable caching if you modify the variable files during a playbook run and want the updated
-            result to be available from the next task on.
-          - "Note that setting I(stage) to C(inventory) has the same effect as setting I(cache) to C(true):
-             the variables will be loaded only once (during inventory loading) and the vars plugin will not
-             be called for every task."
-        type: bool
-        default: true
-        version_added: 0.2.0
-        ini:
-          - key: vars_cache
-            section: community.sops
-        env:
-          - name: ANSIBLE_VARS_SOPS_PLUGIN_CACHE
-    extends_documentation_fragment:
-        - ansible.builtin.vars_plugin_staging
-        - community.sops.sops
+        - Loads SOPS-encrytped YAML vars into corresponding groups/hosts in group_vars/ and host_vars/ directories.
+        - Only SOPS-encrypted vars files, with a top-level "sops" key, will be loaded.
+        - Extends host/group vars logic from Ansible core.
+    notes:
+        - SOPS binary must be on path (missing will raise exception).
+        - Only supports YAML vars files (JSON files will raise exception).
+        - Only host/group vars are supported, other files will not be parsed.
+    options: []
 '''
 
+
 import os
-from ansible.errors import AnsibleParserError
+import subprocess
+import yaml
+from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.module_utils._text import to_bytes, to_native, to_text
-from ansible.plugins.vars import BaseVarsPlugin
 from ansible.inventory.host import Host
 from ansible.inventory.group import Group
 from ansible.utils.vars import combine_vars
-from ansible_collections.community.sops.plugins.module_utils.sops import Sops, SopsError
-
-from ansible.utils.display import Display
-display = Display()
 
 
 FOUND = {}
-DECRYPTED = {}
-DEFAULT_VALID_EXTENSIONS = [".sops.yaml", ".sops.yml", ".sops.json"]
 
 
-class VarsModule(BaseVarsPlugin):
+# Import host_group_vars logic for file-walking functions.
+# We'll still need to copy/paste and modify the `get_vars` function
+# and edit below to insert a call to sops cli.
+from ansible.plugins.vars.host_group_vars import VarsModule as HostGroupVarsModule
 
-    def get_vars(self, loader, path, entities, cache=None):
-        ''' parses the inventory file '''
+
+# All SOPS-encrypted vars files will have a top-level key called "sops".
+# In order to determine whether a file is SOPS-encrypted, let's inspect
+# such a key if it is found, and expect the following subkeys.
+SOPS_EXPECTED_SUBKEYS = [
+    "lastmodified",
+    "mac",
+    "version",
+]
+
+
+
+class AnsibleSopsError(AnsibleError):
+    pass
+
+
+
+class VarsModule(HostGroupVarsModule):
+
+
+    def get_vars(self, loader, path, entities, cache=True):
+        """
+        Parses the inventory file and assembles host/group vars.
+
+
+        Lifted verbatim from ansible.plugins.vars.host_group_vars, with a single
+        in-line edit to support calling out to the SOPS CLI for decryption.
+        Only SOPS-encrypted files will be handled.
+        """
+
 
         if not isinstance(entities, list):
             entities = [entities]
 
+
         super(VarsModule, self).get_vars(loader, path, entities)
 
-        def get_option_value(argument_name):
-            return self.get_option(argument_name)
-
-        if cache is None:
-            cache = self.get_option('cache')
 
         data = {}
         for entity in entities:
@@ -110,6 +83,7 @@ class VarsModule(BaseVarsPlugin):
             else:
                 raise AnsibleParserError("Supplied entity must be Host or Group, got %s instead" % (type(entity)))
 
+
             # avoid 'chroot' type inventory hostnames /path/to/chroot
             if not entity.name.startswith(os.path.sep):
                 try:
@@ -118,7 +92,6 @@ class VarsModule(BaseVarsPlugin):
                     b_opath = os.path.realpath(to_bytes(os.path.join(self._basedir, subdir)))
                     opath = to_text(b_opath)
                     key = '%s.%s' % (entity.name, opath)
-                    self._display.vvvv("key: %s" % (key))
                     if cache and key in FOUND:
                         found_files = FOUND[key]
                     else:
@@ -126,29 +99,74 @@ class VarsModule(BaseVarsPlugin):
                         if os.path.exists(b_opath):
                             if os.path.isdir(b_opath):
                                 self._display.debug("\tprocessing dir %s" % opath)
-                                # NOTE: iterating without extension allow retriving files recursively
-                                # A filter is then applied by iterating on all results and filtering by
-                                # extension.
-                                # See:
-                                # - https://github.com/ansible-collections/community.sops/pull/6
-                                found_files = loader.find_vars_files(opath, entity.name, extensions=DEFAULT_VALID_EXTENSIONS, allow_dir=False)
-                                found_files.extend([file_path for file_path in loader.find_vars_files(opath, entity.name)
-                                                    if any(to_text(file_path).endswith(extension) for extension in DEFAULT_VALID_EXTENSIONS)])
+                                found_files = loader.find_vars_files(opath, entity.name)
                                 FOUND[key] = found_files
                             else:
                                 self._display.warning("Found %s that is not a directory, skipping: %s" % (subdir, opath))
 
+
                     for found in found_files:
-                        if cache and found in DECRYPTED:
-                            file_content = DECRYPTED[found]
-                        else:
-                            file_content = Sops.decrypt(found, display=display, get_option_value=get_option_value)
-                            DECRYPTED[found] = file_content
-                        new_data = loader.load(file_content)
-                        if new_data:  # ignore empty files
-                            data = combine_vars(data, new_data)
+                        # BEGIN SOPS-specific logic
+                        if self._is_encrypted_sops_file(found):
+                            new_data = self._decrypt_sops_file(found)
+
+
+                            if new_data:  # ignore empty files
+                                data = combine_vars(data, new_data)
+                        # END SOPS-specific logic
+
 
                 except Exception as e:
                     raise AnsibleParserError(to_native(e))
-
         return data
+
+
+    def _is_encrypted_sops_file(self, path):
+        """
+        Check whether given filename is likely a SOPS-encrypted vars file.
+        Determined by presence of top-level 'sops' key in vars file.
+
+
+        Assumes file is YAML. Does not support JSON files.
+        """
+        is_sops_file_result = False
+        with open(path, 'r') as f:
+            y = yaml.safe_load(f)
+            if type(y) == dict:
+                # All SOPS-encrypted vars files will have top-level "sops" key.
+                if 'sops' in y.keys() and type(y['sops'] == dict):
+                    if all(k in y['sops'].keys() for k in SOPS_EXPECTED_SUBKEYS):
+                        is_sops_file_result = True
+            return is_sops_file_result
+
+
+    def _decrypt_sops_file(self, path):
+        """
+        Shells out to `sops` binary and reads decrypted vars from stdout.
+        Passes back dict to vars loader.
+
+
+        Assumes that a file is a valid SOPS-encrypted file. Use function
+        `is_encrypted_sops_file` to check.
+
+
+        Assumes file is YAML. Does not support JSON files.
+        """
+        cmd = ["sops", "--input-type", "yaml", "--decrypt", path]
+        real_yaml = None
+        try:
+            decrypted_yaml = subprocess.check_output(cmd)
+        except OSError:
+            msg = "Failed to call SOPS to decrypt file at {}".format(path)
+            msg += ", ensure sops is installed in PATH."
+            raise AnsibleSopsError(msg)
+        except subprocess.CalledProcessError:
+            msg = "Failed to decrypt SOPS file at {}".format(path)
+            raise AnsibleSopsError(msg)
+        try:
+            real_yaml = yaml.safe_load(decrypted_yaml)
+        except yaml.parser.ParserError:
+            msg = "Failed to parse YAML from decrypted SOPS file at {},".format(path)
+            msg += " confirm file is YAML format."
+            raise AnsibleSopsError(msg)
+        return real_yaml
