8812f3eb037407e645b7f7ca496d2c6acda885eb
Alexander Jacocks <alexander@redhat.com>
2021-07-13 14:33:54 -0400

    first commit to use real certs

--- container_security/admin.sh
+++ container_security/admin.sh
@@ -2,7 +2,7 @@
 
 host=$( grep -A1 '\[admin_rhel_node\]' inventory/hosts | tail -1 | sed 's/ .*//' )
 ip=$( grep -A1 '\[admin_rhel_node\]' inventory/hosts | tail -1 | sed 's/.*ansible_ssh_host=//' )
-workshop=$( echo $host | awk -F\. '{ print $1 }' )
+workshop=$( echo $host | awk -F\. '{ print $2 }' )
 
 echo "Logging into $host..."
 
